0db9e6477bb2bc0d5dc666eb7091524db50f7df4
shengwen166 <shengwen16@gmail.com>
2025-05-22 22:51:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -70,5 +70,7 @@ def lark_callback():
 
     return jsonify({"status": "Zoho updated", "zoho_response": zoho_response.json()}), 200
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
